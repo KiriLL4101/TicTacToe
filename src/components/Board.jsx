ebc4275eaae67e08d6646703271c799164b1923e
kiril <fedechkin-kiril@mail.ru>
2020-07-14 02:50:16 +0300

    add render board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,76 +1,133 @@
 import React from "react";
 import Square from "./Square";
 
-export default function Board() {
+export default function Board({ value }) {
   const [state, setState] = React.useState({
-    squares: Array(9).fill(null),
+    squares: value,
     xIsNext: true,
   });
 
-  function handleClick(i) {
+  function handleClick(i, j) {
     const squares = state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = state.xIsNext ? "X" : "O";
+
+    squares[i][j] = state.xIsNext ? "X" : "O";
     setState({
       squares: squares,
       xIsNext: !state.xIsNext,
     });
   }
-  function renderSquare(i) {
-    return <Square value={state.squares[i]} onClick={() => handleClick(i)} />;
+  function renderSquare(i, j) {
+    return (
+      <Square
+        value={state.squares[i][j]}
+        onClick={() => {
+          handleClick(i, j);
+        }}
+      />
+    );
   }
+  function getColsWinner(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (result[j] === undefined) {
+          result[j] = [];
+        }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
+        result[j][i] = arr[i][j];
       }
     }
-    return null;
+    return result.map((item) => {
+      return item.fill("X");
+    });
   }
 
-  let status = "Следующий игрок: " + (state.xIsNext ? "X" : "O");
-  const winner = calculateWinner(state.squares);
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (state.xIsNext ? "X" : "O");
+  function getFirstDiagonal(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (result[i + j] === undefined) {
+          result[i + j] = [];
+        }
+        result[i + j].push(arr[i][j]);
+      }
+    }
+    return result.map((item) => {
+      return item.fill("X");
+    });
   }
+
+  function getSecondDiagonal(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = arr[i].length - 1; j >= 0; j--) {
+        if (result[i] === undefined) {
+          result[i] = [];
+        }
+        result[i].push(arr[i][j]);
+      }
+    }
+    return result.map((item) => {
+      return item.fill("X");
+    });
+  }
+
+  function isWin(check) {
+    for (let i = 0; i < state.squares.length; i++) {
+      for (let j = 4; j < state.squares[i].length; j++) {
+        if (
+          state.squares[i][j] === check[i][j] &&
+          state.squares[i][j] === check[i][j - 1] &&
+          state.squares[i][j] === check[i][j - 2] &&
+          state.squares[i][j] === check[i][j - 3] &&
+          state.squares[i][j] === check[i][j - 4]
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+
+  function calculateWinner(squares) {
+    const colsNum = getColsWinner(squares);
+    const firstDia = getFirstDiagonal(squares);
+    const secondDia = getSecondDiagonal(squares);
+    const lines = [...colsNum, ...firstDia, ...secondDia]
+    if (isWin(lines)) {
+      alert(1)
+      setState({
+        xIsNext: true,
+        squares:state.squares.map((item) => {
+          return item.fill(null);
+        })
+      });
+    }
+    return false;
+  }
+  React.useEffect(() => {
+    calculateWinner(state.squares);
+  }, [state]);
+  let status = "Следующий игрок: " + (state.xIsNext ? "X" : "O");
+
+  function renderBoard(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result[i] = [];
+      for (let j = 0; j < arr[i].length; j++) {
+        result[i][j] = renderSquare(i, j);
+      }
+    }
+    return result;
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {renderBoard(value).map((item) => (
+        <div className="board-row">{item.map((val) => val)}</div>
+      ))}
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import Board from "./Board";
 
 export default function Game() {
+  function sizeBoard(size) {
+    let rowsNum = size;
+    let colsNum = size;
+    let arr = [];
+    for (let i = 0; i < rowsNum; i++) {
+      arr[i] = [];
+      for (let j = 0; j < colsNum; j++) {
+        arr[i][j] = null;
+      }
+    }
+    return arr;
+  }
+
+  const [state, setstate] = React.useState(sizeBoard(6));
+
+  
+  
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board value={state} />
       </div>
       <div className="game-info">
         <div>{/* status */}</div>
